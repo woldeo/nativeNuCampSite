1cccdebfb98d20897d7177ddb1a5c59860a2c89b
Erik Wold <withakmusic@gmail.com>
2020-06-03 13:09:18 -0700

    Modal

--- components/ReservatioComponent.js
+++ components/ReservatioComponent.js
@@ -7,6 +7,7 @@ import {
   Picker,
   Switch,
   Button,
+  Modal,
 } from "react-native";
 import DatePicker from "react-native-datepicker";
 
@@ -17,6 +18,7 @@ class Reservation extends Component {
       campers: 1,
       hikeIn: false,
       date: "",
+      showModal: false,
     };
   }
 
@@ -24,12 +26,21 @@ class Reservation extends Component {
     title: "Reserve Campsite",
   };
 
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
   handleReservation() {
     console.log(JSON.stringify(this.state));
+    this.toggleModal();
+  }
+
+  resetForm() {
     this.setState({
       campers: 1,
       hikeIn: false,
       date: "",
+      showModal: false,
     });
   }
 
@@ -38,8 +49,8 @@ class Reservation extends Component {
       <ScrollView>
         <View style={styles.formRow}>
           <Text style={styles.formLabel}>Number of Campers</Text>
-                <Picker
-                    style={styles.formItem}
+          <Picker
+            style={styles.formItem}
             selectedValue={this.state.campers}
             onValueChange={(itemValue) => this.setState({ campers: itemValue })}
           >
@@ -53,8 +64,8 @@ class Reservation extends Component {
         </View>
         <View style={styles.formRow}>
           <Text style={styles.formLabel}>Hike-In?</Text>
-                <Switch
-                    style={styles.formItem}
+          <Switch
+            style={styles.formItem}
             value={this.state.hikeIn}
             trackColor={{ true: "#5637dd", false: null }}
             onValueChange={(value) => this.setState({ hikeIn: value })}
@@ -62,8 +73,8 @@ class Reservation extends Component {
         </View>
         <View style={styles.formRow}>
           <Text style={styles.formLabel}>Date</Text>
-                <DatePicker
-                    style={{flex: 2, marginRight: 20}}
+          <DatePicker
+            style={{ flex: 2, marginRight: 20 }}
             date={this.state.date}
             format="YYYY-MM-DD"
             mode="date"
@@ -93,7 +104,27 @@ class Reservation extends Component {
             color="#5637dd"
             accessibilityLabel="Tap me to search for available campsites to reserve"
           />
-        </View>
+            </View>
+            <Modal
+                animationType={'slide'}
+                transparent={false}
+                visible={this.state.showModal}
+                onRequestClose={() => this.toggleModal()}>
+                <View style={styles.modal}>
+                    <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
+                    <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text>
+                    <Text style={styles.modalText}>Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}</Text>
+                    <Text style={styles.modalText}>Date: {this.state.date}</Text>
+                    <Button
+                        onPress={() => {
+                            this.toggleModal();
+                            this.resetForm();
+                        }}
+                        color='#5637dd'
+                        title='Close'
+                        />
+                </View>
+                </Modal>
       </ScrollView>
     );
   }
@@ -113,6 +144,22 @@ const styles = StyleSheet.create({
   },
   formItem: {
     flex: 1,
+  },
+  modal: {
+    justifyContent: "center",
+    margin: 20,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    backgroundColor: "#5637dd",
+    textAlign: "center",
+    color: "#fff",
+    marginBottom: 20,
+  },
+  modalText: {
+    fontSize: 18,
+    margin: 10,
   },
 });
 
